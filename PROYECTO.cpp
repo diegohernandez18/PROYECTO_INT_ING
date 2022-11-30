834f47021c2ea1eb2a2b4bfc8f48161ce681b17f
diegohernandez18 <berth.2618.hz@gmail.com>
2022-11-29 21:31:04 -0600

    segunda actualizacion del codigo

--- PROYECTO.cpp
+++ PROYECTO.cpp
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main()
 {
-	int n,r,l;
+	int tamanio,r,l;
+	printf("Este programa puede dibujar rectangulos del tamanio que decidas");
 	printf("Introduce el tamanio de la figura: ");
-	scanf_s("%i", &n);
+	scanf_s("%i", &tamanio);
 	printf("\n");
 
-	for (r = 1; r <= n; r++)
+	for (r = 1; r <= tamanio; r++)
 	{
-		for (l = 1; l <= n; l++)
+		for (l = 1; l <= tamanio; l++)
 		{
 			printf("*");
 		}
-	}
+		    printf("\n");
+	}       
 	return 0;
 }
